5ff658ad49d39fa0866bf701a3d521a03e4cbc11
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-09-28 16:35:06 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from 'express'
 
+interface User {
+  name: string
+  email: string
+}
+
 const app = express()
 
-app.get('/users', (request:Request, response:Response):void => {
-  response.send('hi')
-})
+const array: User[] = []
+
+app.use(express.json())
+
+app.get(
+  '/users',
+  (request: Request, response: Response): Response => {
+    return response.json(array)
+  },
+)
+app.post(
+  '/users',
+  (request: Request, response: Response): Response => {
+    const { name, email } = request.body
+
+    array.push({ name, email })
+
+    return response.json({ name, email })
+  },
+)
 
 app.listen(3333, () => console.log('Sever On'))
