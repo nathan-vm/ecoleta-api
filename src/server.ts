9cbb39ffc88c77c427d4ae21b440b34120e3d2b3
Nathan Vieira <nathanvieira@lett.digital>
2020-12-29 14:18:33 -0300

    feat(docker-compose): add docker-compose to the project

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,13 @@
 import express, { Request, Response } from 'express'
 
-interface User {
-  name: string
-  email: string
-}
-
 const app = express()
-
-const array: User[] = []
 
 app.use(express.json())
 
 app.get(
-  '/users',
-  (request: Request, response: Response): Response => {
-    return response.json(array)
+  '/',
+  (_request: Request, response: Response): Response => {
+    return response.json({ message: 'Hello World' })
   },
 )
-app.post(
-  '/users',
-  (request: Request, response: Response): Response => {
-    const { name, email } = request.body
-
-    array.push({ name, email })
-
-    return response.json({ name, email })
-  },
-)
-
 app.listen(3333, () => console.log('Sever On'))
